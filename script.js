7b017af54fe1a4a54dd50e492afad6f023347434
MatthewPollardUK <matthewpoll@live.com>
2022-06-16 16:45:03 +0100

    retrieve data ssuccessful in JS

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ let newId;
 
 
 let changeQuote = () => {
-randomIdGet();
-console.log(randomId)
+randomIdGet(); //get random ID number
+//console.log(randomId)
+dataRetrieve();
 }
 
 changeButton.addEventListener('click', changeQuote);
@@ -17,16 +18,18 @@ changeButton.addEventListener('click', changeQuote);
 
 const randomIdGet = () => {
   newId = Math.floor(Math.random() * 10) + 1;
-  // if (newId !== randomId){
-  // randomId = newId;
-  // console.log(randomId);
- //} else {
-  //console.log(`newId = ${newId} and randomId = ${randomId} are the same`);
-//  randomIdGet();
-//return randomId;
-//}
-
 newId !== randomId ? randomId = newId : randomIdGet();
+}
 
+const dataRetrieve = () => {
+  let requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
 
+  let urlToUse = `https://testimonialapi.toolcarton.com/api/${randomId}`;
+fetch(urlToUse, requestOptions)
+ .then(response => response.text())
+ .then(result => console.log(result))
+ .catch(error => console.log('error', error));
 }
